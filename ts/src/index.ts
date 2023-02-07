ba6cdf19c616146094bc11905903cb89efd0e30a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-07 16:14:12 +0900

    Add ts todo

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -5,3 +5,16 @@ interface TodoType {
   category: string;
   tags?: string[];
 }
+
+interface TodoList {
+  todos: TodoType[];
+
+  createTodo: (todo: TodoType) => void;
+  findById: (id: number) => TodoType;
+  updateTodo: (todo: TodoType) => void;
+  updateTodoTag: (id: number, tagIndex: number, tag: string) => void;
+  deleteTodo: (id: number) => void;
+  resetTodo: () => void;
+  deleteTag: (todoIndex: number, tagIndex: number) => void;
+  deleteAllTag: (todoIndex: number) => void;
+}
