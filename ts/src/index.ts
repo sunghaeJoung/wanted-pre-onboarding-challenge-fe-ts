0acd623ff1b05857a0c934c1345474f050d12e89
sunghaeJoung <shh03288@gmail.com>
2023-02-11 23:37:50 +0900

    설계를 바탕으로 코드 구현

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -18,3 +18,44 @@ interface TodoList {
   deleteTag: (todoIndex: number, tagIndex: number) => void;
   deleteAllTag: (todoIndex: number) => void;
 }
+
+let todoList: TodoType[] = []
+
+const createTodo = (todo: TodoType): void => {
+  todoList.push(todo);
+}
+
+// 피드백. 찾고자 하는 값이 없을 때 return값이 undefined일 수가 있음 
+const findById = (id: number): TodoType | undefined => {
+  return todoList.find(todo => todo.id === id)
+}
+
+const updateTodo = (todo: TodoType): void => {
+  const targetIndex = todoList.findIndex(el => el.id === todo.id);
+  if (!targetIndex) return;
+  todoList[targetIndex] = todo;
+}
+
+const updateTodoTag = (id: number, tagIndex: number, tag: string): void => {
+  const targetIndex = todoList.findIndex(el => el.id === id);
+
+  if (!targetIndex) return;
+  (todoList[targetIndex]?.tags || []).splice(tagIndex, 1, tag);
+}
+
+const deleteTodo = (id: number): void => {
+  todoList = todoList.filter(el => el.id !== id);
+}
+
+const resetTodo = (): void => {
+  todoList = [];
+}
+
+// 피드백. todoIndex가 아니라 todoId가 되어야하지 않을까?
+const deleteTag = (todoIndex: number, tagIndex: number): void => {
+  todoList[todoIndex].tags = (todoList[todoIndex]?.tags || []).filter((_, index) => index !== tagIndex)
+}
+// 피드백. 위와 동일
+const deleteAllTag = (todoIndex: number): void => {
+  todoList[todoIndex].tags = [];
+};
